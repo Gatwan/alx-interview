7a6b47d02866e1635481d16af916768760bd9104
Gatwan <abigailgathigia@gmail.com>
2023-09-02 08:12:10 +0300

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,24 +6,22 @@ def validUTF8(data):
     """ Checks data set """
     num_bytes = 0
 
-    for num in data:
-        # Checks the current integer if it starts a valid UTF-8 character sequence
+    for byte in data:
         if num_bytes == 0:
-            if (num >> 7) == 0b0:
-                num_bytes = 0
-            elif (num >> 5) == 0b110:
+            if byte < 128:
+                continue
+
+            if 192 <= byte < 224:
                 num_bytes = 1
-            elif (num >> 4) == 0b1110:
+            elif 224 <= byte <240:
                 num_bytes = 2
-            elif (num >> 3) == 0b11110:
-                num_bytes = 3
+            elif 240 <= byte < 248:
+                num_byes = 3
             else:
                 return False
         else:
-            #Check if curret integer starts with the pattern '10'
-            if (num >> 6) != 0b10:
+            if 128 <= byte < 192:
+                num_bytes -= 1
+            else:
                 return False
-            num_bytes -= 1
-
-        # If all bytes are correctly processed, bytes should not remain
-        return num_bytes == 0
+    return True
